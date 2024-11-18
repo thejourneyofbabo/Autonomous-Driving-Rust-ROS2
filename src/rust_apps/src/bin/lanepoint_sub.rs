19b492894f220ff79fc0beea86bb545ccfdceb50
thejourneyofbabo <journeyofbabo@gmail.com>
2024-11-19 01:33:37 +0900

    little change

--- src/rust_apps/src/bin/lanepoint_sub.rs
+++ src/rust_apps/src/bin/lanepoint_sub.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Error> {
     let context = Context::new(env::args())?;
     let node = rclrs::create_node(&context, "lane_points_subscriber")?;
 
-    let _subscription = node.create_subscription::<LanePointData, _>(
+    let _subscription = node.create_subscription(
         "/ego/lane_points",
         rclrs::QOS_PROFILE_DEFAULT,
         move |msg: LanePointData| {
@@ -17,6 +17,16 @@ fn main() -> Result<(), Error> {
             println!("---");
         },
     )?;
+    //let _subscription = node.create_subscription::<LanePointData, _>(
+    //    "/ego/lane_points",
+    //    rclrs::QOS_PROFILE_DEFAULT,
+    //    move |msg: LanePointData| {
+    //        for point in msg.point.iter() {
+    //            println!("x: {}, y: {}, z: {}", point.x, point.y, point.z);
+    //        }
+    //        println!("---");
+    //    },
+    //)?;
 
     rclrs::spin(node).map_err(|err| err.into())
 }
