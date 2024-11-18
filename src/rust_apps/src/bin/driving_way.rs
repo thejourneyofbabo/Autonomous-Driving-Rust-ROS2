30a786c233281e951ff46f03ab25e58f549f661c
thejourneyofbabo <journeyofbabo@gmail.com>
2024-11-19 01:33:24 +0900

    making driving way

---
+++ src/rust_apps/src/bin/driving_way.rs
@@ -0,0 +1,22 @@
+use ad_msgs::msg::LanePointData;
+use anyhow::{Error, Result};
+use rclrs::Context;
+use std::env;
+
+fn main() -> Result<(), Error> {
+    let context = Context::new(env::args())?;
+    let node = rclrs::create_node(&context, "lane_points_subscriber")?;
+
+    let _subscription = node.create_subscription(
+        "/ego/lane_points",
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: LanePointData| {
+            for point in msg.point.iter() {
+                println!("x: {}, y: {}, z: {}", point.x, point.y, point.z);
+            }
+            println!("---");
+        },
+    )?;
+
+    rclrs::spin(node).map_err(|err| err.into())
+}
