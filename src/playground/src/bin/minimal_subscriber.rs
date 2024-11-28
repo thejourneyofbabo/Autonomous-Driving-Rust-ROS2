4af73b1cc17294f5a510bbc83c7221990becc90c
thejourneyofbabo <journeyofbabo@gmail.com>
2024-11-28 09:29:27 +0900

    minimal nodes

---
+++ src/playground/src/bin/minimal_subscriber.rs
@@ -0,0 +1,21 @@
+use std::env;
+
+use anyhow::{Error, Result};
+
+fn main() -> Result<(), Error> {
+    let context = rclrs::Context::new(env::args())?;
+    let node = rclrs::create_node(&context, "minimal_subscriber")?;
+    let mut num_messages: usize = 0;
+
+    let _subscription = node.create_subscription(
+        "publish_hello",
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: std_msgs::msg::String| {
+            num_messages += 1;
+            println!("I heard: '{}'", msg.data);
+            println!("Got {} messages so far)", num_messages);
+        },
+    )?;
+
+    rclrs::spin(node).map_err(|err| err.into())
+}
